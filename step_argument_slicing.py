38a15b7a1243a5a327e36ebd84c4d78528f606ee
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-01-25 12:39:42 +0530

    string

---
+++ step_argument_slicing.py
@@ -0,0 +1,16 @@
+#slicing/selecting sub sequences
+#lang = "python"
+#syntax-[step argument]
+#print ("mukul"[0:5:2])
+
+
+
+#negative step argument
+#print ("mukul"[-1::-1]) 
+
+
+
+
+name= input ("enter your name:")
+reverse = name [-1::-1]
+print (f"reverse of your name is {reverse}")
---
+++ string_slicing.py
@@ -0,0 +1,17 @@
+#slicing/selecting sub sequences
+#lang = "python"
+#syntax[start argument: stop argument -1]
+#print ("mukul"[3:5])
+
+#no argument
+#print ("mukul"[:])
+
+#start argument
+#print("mukul"[2:])
+
+#stop argument
+#print("mukul"[:3])
+
+
+
+print("mukul"[0:5:2])
