0859dbd0d803be213abe1f8ccca2cee28e7f0bf4
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-02-07 23:37:29 +0530

    check

---
+++ check_empty_or_not.py
@@ -0,0 +1,5 @@
+name = input ("enter your name:")
+if name: #TRUE IF STRING IS NOT EMPTY
+    print (F"your name is {name}")
+else:
+    print ("you didn\'t type anything")
---
+++ the_in_keyword.py
@@ -0,0 +1,7 @@
+#in_keywords:-
+
+name = "rahul"
+if 'a' in "rahul":
+    print("a is present in name")
+else:
+    print("a is not present in name")
