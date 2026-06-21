f82ddd8a4b919af8c1176e9cb987902b06727fdb
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-06-22 00:33:49 +0530

    find

---
+++ #functions_practice.py
@@ -0,0 +1,14 @@
+# def last_char(name: str) -> str:
+#     return name[-1]
+
+#  print(last_char("mukul kushwaha"))
+
+
+def odd_even(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+    
+print(odd_even(4))
+ 
