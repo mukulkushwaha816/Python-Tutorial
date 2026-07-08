bc4609e911fd8c87404948ee7dc8dfe0fbed2f83
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-07-08 22:55:59 +0530

    list

---
+++ list2.py
@@ -0,0 +1,8 @@
+movies = []
+mov1=input("enter 1st movie:")
+movies.append(mov1)
+mov2=input("enter 2nd movie:")
+movies.append(mov2)
+mov3=input("enter 3rd movie:")
+movies.append(mov3)
+print(movies)
