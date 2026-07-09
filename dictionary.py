348963a3091f4a4a088595d29764f0065b4583ef
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-07-09 17:08:07 +0530

    dict

--- dictionary.py
+++ dictionary.py
@@ -5,14 +5,14 @@
 # }
 # print (student)
 
-#or 
+# or 
 
 # user = {'name':'mukul','age':'20','course':'bca'}
 # print(user)
 # print(type(user))
 
-#or
-#how to add data to empty dictionary
+# #or
+# how to add data to empty dictionary
 user_info2={}
 user_info2['name']='mukul'
 user_info2['age']='20'
