b7bd2b3255ee38649b2e9bfc0bf03d846769405b
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-07-09 11:41:15 +0530

    changes

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,19 @@
-student = {
-"name": "mukuul",
-"age": "20",
-"course": "bca",
-}
-print (student)
+# student = {
+# "name": "mukuul",
+# "age": "20",
+# "course": "bca",
+# }
+# print (student)
+
+#or 
+
+# user = {'name':'mukul','age':'20','course':'bca'}
+# print(user)
+# print(type(user))
+
+#or
+#how to add data to empty dictionary
+user_info2={}
+user_info2['name']='mukul'
+user_info2['age']='20'
+print(user_info2)
