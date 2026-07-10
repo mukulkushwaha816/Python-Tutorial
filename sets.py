3d856bb78b0bd53df0a51fabe49d2b870cdb5abf
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-07-10 23:42:48 +0530

    set

---
+++ sets.py
@@ -0,0 +1,12 @@
+marks = {}
+
+x = int(input("enter phy : "))
+marks.update({"phy" : x})
+
+x = int(input("enter cheme : "))
+marks.update({"chem" : x})
+
+x = int (input("enetr math  : "))
+marks.update({"math" : x})
+
+print(marks)
