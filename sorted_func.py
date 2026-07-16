15c69cc0f995f6e3a6a7353a60af90a2cca0d2d6
Mukul Kushwaha <mukulkushwaha816@gmail.com>
2026-07-16 20:24:13 +0530

    func

---
+++ sorted_func.py
@@ -0,0 +1,8 @@
+guitars=[
+    {'model':'yamaha f310','price':8400},
+    {'model':'faith napyune','price':50000},
+    {'model':'faith apollo venus','price':35000},
+
+    ]
+print(sorted(guitars,key = lambda d:d['price']))
+    
